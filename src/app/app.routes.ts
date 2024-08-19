6df6c692cd3bde542a06d080f8484a9210e34fab
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-08-19 16:58:41 +0530

    rrrt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ProductslistComponent } from './productslist/productslist.component';
+import { NotFoundComponentComponent } from './not-found-component/not-found-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutusComponent },
+    { path: 'products', component: ProductslistComponent },
+    {path:'**',component:NotFoundComponentComponent}
+  ];
+
---
+++ src/app/productslist/productslist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productslist',
+  standalone: true,
+  imports: [],
+  templateUrl: './productslist.component.html',
+  styleUrl: './productslist.component.css'
+})
+export class ProductslistComponent {
+
+}
