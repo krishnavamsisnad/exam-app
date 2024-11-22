811c32d1bcd4825bcb5c6c3321fcb25346ba08ae
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-11-22 12:07:24 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-
+import { NgbNavModule, NgbProgressbarModule, NgbTimepickerModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgbNavModule,NgbProgressbarModule,NgbTimepickerModule,FormsModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'exam-app';
+  active = 1;
+  time = { hour: 13, minute: 30 };
   
 }
